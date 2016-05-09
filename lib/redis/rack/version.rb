f96f37aa9b6618e47861b5f415a4e9b3d07be01b
Tom Scott <tscott@weblinc.com>
2016-05-09 11:49:23 -0400

    v2.0.0.pre Add support for Rails 5 and Rack 2.

--- lib/redis/rack/version.rb
+++ lib/redis/rack/version.rb
@@ -1,6 +1,6 @@
 class Redis
   module Rack
-    VERSION = '2.0.0'
+    VERSION = '2.0.0.pre'
   end
 end
 
--- test/redis/rack/version_test.rb
+++ test/redis/rack/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Redis::Rack::VERSION do
   it 'returns current version' do
-    Redis::Rack::VERSION.must_equal '1.5.0'
+    Redis::Rack::VERSION.must_equal '2.0.0.pre'
   end
 end
